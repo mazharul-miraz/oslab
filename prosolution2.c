957f157f8a27a6cce2b50ab32702fe5352866ae9
mazharul.miraz <mazharul.miraz@outlook.com>
2019-04-09 00:54:50 +0530

    2 updated

--- prosolution2.c
+++ prosolution2.c
@@ -13,3 +13,27 @@ int main()
        scanf("%d",&bt[count]);
        rt[count]=bt[count];
       }
+    printf("Enter Tm Quantum:\t");
+    scanf("%d",&tq);
+    printf("\n\nProcess\t|Turnaround Tm|Waiting Tm\n\n");
+    for(tm=0,count=0;rem!=0;)
+    {
+        if(rt[count]<=tq && rt[count]>0)
+       {
+           tm+=rt[count];
+           rt[count]=0;
+           flag=1;
+        }
+        else if(rt[count]>0)
+       {
+           rt[count]-=tq;
+           tm+=tq;
+         }
+       if(rt[count]==0 && flag==1)
+      {
+          rem--;
+          printf("P[%d]\t|\t%d\t|\t%d\n",count+1,tm-at[count],tm-at[count]-bt[count]);
+          wt+=tm-at[count]-bt[count];
+          trnarndtm+=tm-at[count];
+          flag=0;
+        }
